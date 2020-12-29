3e442b57fd7da6b37e9266820f3eb0abe59bcf21
Olivier kessler <olivier.kessler@protonmail.com>
2020-12-29 13:14:19 +0100

    Fix more bugs

--- src/phpgenerator.c
+++ src/phpgenerator.c
@@ -4,6 +4,7 @@
  */
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_JSON_NESTING 30
 #define PHP_NAME_MAX_LENGTH 20
@@ -43,13 +44,13 @@ char* generate_php_class(struct php_class* php_class) {
 	
 	for(int i = 0; i < php_class->number_methods; i++) {
 		strcat(result,"function ");
-		strcat(result, php_class->methods[i]->name);
+		strcat(result, php_class->methods[i].name);
 		strcat(result, " {}\n");
 	}	
 
 	for(int i = 0; i < php_class->number_members; i++) {
 		strcat(result, "$");
-		strcat(result, php_class->members[i]->name);
+		strcat(result, php_class->members[i].name);
 		strcat(result, ";\n");
 	}
 
@@ -66,39 +67,44 @@ int map_json_to_php_classes(char *json, size_t size_json, struct php_class* php_
 	int php_object_index = 0;
 	bool bracket_opened = 0;
 	bool braces_opened = 0;
-	char classname[PHP_NAME_MAX_LENGTH] = "";
+	char classname[PHP_NAME_MAX_LENGTH];
 
 	for(int i = 0; i < size_json; i++){
-		if(json[i] == "{") {
+		if(json[i] == '{') {
+			if(braces_opened == 1){
+				return 0; //Nested objects not supported
+			}
 			braces_opened = 1;
 			php_object_index += 1;
-			php_classes[php_object_index] = {}
-		} else if (json[i] == "}") {
+		} else if (json[i] == '}') {
+			if(braces_opened == 0){
+				return 0; //Nested objects not supported
+			}
 			braces_opened = 0;
-		} else if (json[i] == ":") {
+		} else if (json[i] == ':') {
 			if (braces_opened = 0) {
 				return 0; //Bad JSON formating
 			}
-		} else if (json[i] == "[") {
+		} else if (json[i] == '[') {
 			if(bracket_opened == 1) {
 				return 0; //Nested arrays not supported
 			}
 			bracket_opened = 1;
-		} else if (json[i] == "]") {
+		} else if (json[i] == ']') {
 			if(bracket_opened == 0) {
 				return 0; //Nested arrays not supported
 			}
 			bracket_opened = 0;
-		} else if (json[i] == "c" && json[i+1] == "l" && json[i+2] == "a"
-				&& json[i+3] == "s" && json[i+4] == "s" && (json[i+5] == ":" || json[i+6] = ":")) { //class name
+		} else if (json[i] == 'c' && json[i+1] == 'l' && json[i+2] == 'a'
+				&& json[i+3] == 's' && json[i+4] == 's' && (json[i+5] == ':' || json[i+6] == ':')) { //class name
 			
 			int j = 0;
 			
-			while( json[j] != "\n") {
+			while( json[j] != '\n') {
 				strcat(classname, json[j]);
 			}
 
-			php_classes[php_object_index]->name = classname;
+			strcpy(php_classes[php_object_index].name, classname);
 		}
 
 	}	
@@ -123,7 +129,7 @@ char* jsontophp(char *json, size_t size_json, char *conversion_result) {
 	number_of_classes = map_json_to_php_classes(json, size_json, php_classes);
 	
 	for(int i = 0; i < number_of_classes; i++) {
-		strcat(result_string, generate_php_class(php_classes[i]));
+		strcat(result_string, generate_php_class(&php_classes[i]));
 	}
 
 	return result_string;
