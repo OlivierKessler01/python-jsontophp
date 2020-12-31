96229967cfaee74c35b4ddf25a6025b97209e94a
Olivier kessler <olivier.kessler@protonmail.com>
2020-12-31 18:24:01 +0100

    Fix php class generation

--- src/phpgenerator.c
+++ src/phpgenerator.c
@@ -37,15 +37,15 @@ struct json_object {
 /**
  * Generates a string containing a full synthax-complient, PHP class
  */
-char* generate_php_class(struct php_class* php_class) {
-	char result[300] = "class ";
+char* generate_php_class(struct php_class* php_class, char result[300]) {
+	strcat(result, "class ");
 	strcat(result, php_class->name);
-	strcat(result, " {");
+	strcat(result, "\n{");
 	
 	for(int i = 0; i < php_class->number_methods; i++) {
 		strcat(result,"function ");
 		strcat(result, php_class->methods[i].name);
-		strcat(result, " {}\n");
+		strcat(result, "\n{\n}\n");
 	}	
 
 	for(int i = 0; i < php_class->number_members; i++) {
@@ -54,7 +54,7 @@ char* generate_php_class(struct php_class* php_class) {
 		strcat(result, ";\n");
 	}
 
-	strcat(result, "}\n");
+	strcat(result, "\n}\n");
 	
 	return result;
 }
@@ -62,20 +62,23 @@ char* generate_php_class(struct php_class* php_class) {
 /**
  * Generates php_class objects from a JSON string, and return the number of php classes
  */
-int map_json_to_php_classes(char *json, size_t size_json, struct php_class* php_classes) {
+int map_json_to_php_classes(char json[MAX_JSON_CHARACTERS], size_t size_json, struct php_class* php_classes) {
 	int number_of_classes = 0;
 	int php_object_index = 0;
 	bool bracket_opened = 0;
 	bool braces_opened = 0;
+	int j = 0;
 	char classname[PHP_NAME_MAX_LENGTH];
 
-	for(int i = 0; i < size_json; i++){
+	for(int i = 0; i < strlen(json); i++){
 		if(json[i] == '{') {
 			if(braces_opened == 1){
 				return 0; //Nested objects not supported
 			}
 			braces_opened = 1;
-			php_object_index += 1;
+			if(php_object_index != 0) {
+				php_object_index += 1;
+			}
 		} else if (json[i] == '}') {
 			if(braces_opened == 0){
 				return 0; //Nested objects not supported
@@ -95,15 +98,27 @@ int map_json_to_php_classes(char *json, size_t size_json, struct php_class* php_
 				return 0; //Nested arrays not supported
 			}
 			bracket_opened = 0;
-		} else if (json[i] == 'c' && json[i+1] == 'l' && json[i+2] == 'a'
-				&& json[i+3] == 's' && json[i+4] == 's' && (json[i+5] == ':' || json[i+6] == ':')) { //class name
-			
-			int j = 0;
-			
-			while( json[j] != '\n') {
-				strcat(classname, json[j]);
+		} else if (json[i] == 'c' && json[i+1] == 'l' && json[i+2] == 'a' && json[i+3] == 's' && json[i+4] == 's') { //class name
+		
+			if(json[i+5] == ':'){
+				j = i + 6;
+			} else if (json[i+6] == ':') {
+				j = i + 7;
+			} else {
+				continue;
+			}	
+
+			//Writes the classname
+			while(j < strlen(json) && json[j] != EOF &&  json[j] != '\n' && json[j] != '}') {
+				if(json[j] != ' '){
+					strncat(classname, &json[j], 1);
+				}
+
+				j = j + 1;
 			}
 
+			number_of_classes = number_of_classes + 1;
+			i = j - 1;
 			strcpy(php_classes[php_object_index].name, classname);
 		}
 
@@ -116,29 +131,31 @@ int map_json_to_php_classes(char *json, size_t size_json, struct php_class* php_
 /*
  *This functions converts a JSON string into a string of PHP classes
  */
-char* jsontophp(char *json, size_t size_json, char *conversion_result) {
-	char result_string[1000] = "";
+char* jsontophp(char json[MAX_JSON_CHARACTERS], size_t size_json, char conversion_result[1000]) {
+	
 	struct php_class php_classes[1000];
 	int number_of_classes;
+	char php_class_string[300];
 
 	if(size_json > 1000) {
-		sprintf(result_string, "JSON input is too long, max number of characters is %d", MAX_JSON_CHARACTERS);
-		return result_string;
+		sprintf(conversion_result, "JSON input is too long, max number of characters is %d", MAX_JSON_CHARACTERS);
+		return conversion_result;
 	}
 	
 	number_of_classes = map_json_to_php_classes(json, size_json, php_classes);
-	
+
 	for(int i = 0; i < number_of_classes; i++) {
-		strcat(result_string, generate_php_class(&php_classes[i]));
+		strcat(conversion_result, generate_php_class(&php_classes[i], php_class_string));
 	}
 
-	return result_string;
+	return conversion_result;
 }
 
 int main(){
 	char conversion_result[1000] = "";
-	char json[1000] = "{class : 'testclass'}";
-		
-	jsontophp(json, strlen(json), conversion_result); 
+	char json[MAX_JSON_CHARACTERS] = "{class : testclass}";	
+	jsontophp(json, strlen(json), conversion_result);
+
+	printf("%s", conversion_result);	
 	return 0;
 }
